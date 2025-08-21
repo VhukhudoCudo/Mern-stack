458adf5955b23a21d10abd4fcdf9814e146c72d2
Vhukhudo <vhukhudocudo@gmail.com>
2025-08-21 22:29:31 +0200

    commit

--- Book-Store/frontend/src/pages/EditBook.jsx
+++ Book-Store/frontend/src/pages/EditBook.jsx
@@ -37,7 +37,7 @@ const EditBook = () => {
     };
     setLoading(true);
     axios
-      .put(`http://localhost:5555/books/${id}`, data)
+      .put(`https://book-store-backend-r1ee.onrender.com/books/${id}`, data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Edited successfully', { variant: 'success' });
